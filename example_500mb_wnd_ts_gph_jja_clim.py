d7c21bd1e1ada0f2e5248151ad762a76565fd71a
Ji-Woo Lee <lee1043@crunchy.llnl.gov>
2016-08-24 12:40:26 -0700

    code clean up

--- example_500mb_wnd_ts_gph_jja_clim.py
+++ example_500mb_wnd_ts_gph_jja_clim.py
@@ -1,102 +1,76 @@
 ################################################################################################# 
-# This python script plots climatologyt of JJA, at 500 hPa, obtained from one single CMIP5 model
+# This Python script plots JJA climatology at 850 hPa level, which given from one of CMIP5 models
 # 
-# Ji-Woo Lee (jwlee@llnl.gov), April 2016
+# Ji-Woo Lee (jwlee@llnl.gov), August 2016
 ################################################################################################# 
 
 import cdms2 as cdms
 import cdutil
 import cdtime
 import vcs
-#from vorticity import *
 from cdms2 import MV
 
-#
-# Define function to get JJA climatology
-#
-def getjja(var):
-    var_jja = cdutil.JJA.climatology(d(time=(start_time,end_time)))
-    var_jja.units = var.units
-    var_jja.long_name = 'JJA clim. '+var.id+' '+str(start_year)+'-'+str(end_year)
-    var_jja.id = d.id
-    var_jja.model = 'HadGEM2-AO'
-    return(var_jja)
-
-#
-# open file from local -- DATA was downloaded from ESGF
-#
+#================================================================================================
+# Data
+#------------------------------------------------------------------------------------------------
+# List of variables to plot ---
 vars = ['zg','ta','ua','va']
 nvar = len(vars)
 
+# Time period ---
 start_year = 1949
 end_year = 2010
 start_time = cdtime.comptime(start_year)
 end_time = cdtime.comptime(end_year)
 
+# Dictionary for variables ---
 data = {}
 
+# Bring data in ---
 for var in vars[0:nvar]:
-    odir = '/cmip5_css02/data/cmip5/output1/NIMR-KMA/HadGEM2-AO/historical/mon/atmos/Amon/r1i1p1/'+var+'/1/' # Put your data directory here
-    nc = var+'_Amon_HadGEM2-AO_historical_r1i1p1_186001-200512.nc'
-    f = cdms.open(odir+nc)
+  print var
 
-    #
-    # load variable
-    #
-    #d = f(var,latitude=(-60,80),longitude=(0,360),lev=50000)
-    d = f(var,lev=50000)
+  # Open file (In this example case, data was downloaded from ESGF. Please prepare your data) ---
+  odir = '/cmip5_css02/data/cmip5/output1/NIMR-KMA/HadGEM2-AO/historical/mon/atmos/Amon/r1i1p1/'+var+'/1/' # Put your data directory here
+  nc = var+'_Amon_HadGEM2-AO_historical_r1i1p1_186001-200512.nc'
+  f = cdms.open(odir+nc)
 
-    #
-    # climatology calculation
-    #
-    data[var] = getjja(d)
+  # Load variable ---
+  d = f(var,lev=50000)
 
-    print var
+  # Calculate JJA seasonal climatology ---
+  data[var] = cdutil.JJA.climatology(d(time=(start_time,end_time)))
 
-    if(var=='zg'):
-        lon = d.getLongitude()
-        lat = d.getLatitude()
-    f.close()
+  # Close file ---
+  f.close()
 
-#
-# Create canvas
-#
+#================================================================================================
+# Plot
+#------------------------------------------------------------------------------------------------
+# Create canvas ---
 canvas = vcs.init(geometry=(1200,800))
 canvas.open()
 template = canvas.createtemplate()
-template.blank(["title","mean","min","max","dataname","crdate","crtime","units"]) ## Turn off additional information
+template.blank(['title','mean','min','max','dataname','crdate','crtime','units']) ## Turn off additional information
 #template.list() ## This commend could give list of items 
 
-#
-# Set ploting range
-#
+# Set ploting range ---
 lat1=-60
 lat2=80
 lon1=0
 lon2=360
 
-#
-# Calculate vorticity field
-#
-#data['vor'] = vorticity.relative(data['ua'],data['va'],lon,lat)
-#
-# Calculate wind speed, instead, for now...
-#
-data['vor'] = MV.sqrt(data['ua']**2+data['va']**2)
-#
-# Vorticity field 
-#
+# Wind speed ---
+data['wnd'] = MV.sqrt(data['ua']**2+data['va']**2)
 iso = canvas.createisofill()
 iso.datawc_x1 = lon1
 iso.datawc_x2 = lon2
 iso.datawc_y1 = lat1
 iso.datawc_y2 = lat2
-canvas.setcolormap("blue_to_orange")
-canvas.plot(data['vor'],iso,template)
+canvas.setcolormap('blue_to_orange')
+canvas.plot(data['wnd'],iso,template)
 
-#
-# Geopotential height field
-#
+# Geopotential height field ---
 lines1 = vcs.createisoline()
 lines1.datawc_x1 = lon1
 lines1.datawc_x2 = lon2
@@ -108,25 +82,19 @@ lines1.label = 'y'
 lines1.textcolors=['black']
 canvas.plot(data['zg'],lines1,template)
 
-#
-# T field
-#
+# T field ---
 lines2 = vcs.createisoline()
 lines2.datawc_x1 = lon1
 lines2.datawc_x2 = lon2
 lines2.datawc_y1 = lat1
 lines2.datawc_y2 = lat2
 lines2.linecolors = ['red']
-#lines2.line=['solid']
 lines2.line=['dot']
-#lines2.line=['dash']
 lines2.label = 'y'
 lines2.textcolors=['red']
 canvas.plot(data['ta'],lines2,template)
 
-#
-# Plot wind field (vector)
-#
+# Plot wind field (vector) ---
 vec = vcs.createvector()
 ua2 = data['ua'][...,::3,::3] ## Resample U field to reduce vector density to half
 va2 = data['va'][...,::3,::3] ## Resample V filed to reduce vector density to half
@@ -136,20 +104,16 @@ vec.datawc_y1 = lat1
 vec.datawc_y2 = lat2
 canvas.plot(ua2,va2,vec,template)
 
-#
-# Set title
-#
+# Set title ---
 plot_title = vcs.createtext()
 plot_title.x = .5
 plot_title.y = .98
 plot_title.height = 24
-plot_title.halign = "center"
-plot_title.valign = "top"
-plot_title.color="black"
-plot_title.string = data['zg'].model+', JJA mean, 500 hPa'
+plot_title.halign = 'center'
+plot_title.valign = 'top'
+plot_title.color='black'
+plot_title.string = 'JJA mean climatology, 500 hPa'
 canvas.plot(plot_title)
 
-#
-# Drop output as image file
-#
-canvas.png("example_500mb_wnd_ts_gph_jja_clim")
+# Save output as image file ---
+canvas.png('example_500mb_wnd_ts_gph_jja_clim')
